aaebe33b2243e735af79a6c1fe8eebf866516389
Dmitriy Tverdiakov <dmitriy.tverdiakov@neo4j.com>
2022-06-22 23:42:28 +0100

    Testing

--- tlsserver/main.go
+++ tlsserver/main.go
@@ -73,7 +73,7 @@ func main() {
 	}
 	// Deadline needed for dotnet, seems to stick to the socket even when TLS handshake failed.
 	// Deadline is a good thing anyway...
-	conn.SetReadDeadline(time.Now().Add(1 * time.Second))
+	conn.SetReadDeadline(time.Now().Add(10 * time.Second))
 	fmt.Printf("TLS, client connected from %s, waiting for Bolt handshake\n", conn.RemoteAddr())
 
 	handshake := make([]byte, 4*5)
